4fe07111d29ed3092d331339f63bb31813ef2034
Matijs van Zuijlen <matijs@matijs.net>
2014-06-27 20:50:46 +0200

    Convert specs to RSpec 2.99.1 syntax with Transpec
    This conversion is done by Transpec 2.3.1 with the following command:
        transpec
    
    * 474 conversions
        from: obj.should
          to: expect(obj).to
    
    * 277 conversions
        from: == expected
          to: eq(expected)
    
    * 122 conversions
        from: obj.should_not
          to: expect(obj).not_to
    
    * 18 conversions
        from: be_false
          to: be_falsey
    
    * 15 conversions
        from: be_true
          to: be_truthy
    
    * 13 conversions
        from: obj.should_receive(:message)
          to: expect(obj).to receive(:message)
    
    * 6 conversions
        from: it { should ... }
          to: it { is_expected.to ... }
    
    * 6 conversions
        from: its(:attr) { }
          to: describe '#attr' do subject { super().attr }; it { } end
    
    * 3 conversions
        from: obj.stub(:message)
          to: allow(obj).to receive(:message)
    
    * 2 conversions
        from: obj.should_not_receive(:message)
          to: expect(obj).not_to receive(:message)
    
    * 1 conversion
        from: obj.should_receive(:message).and_return { value }
          to: obj.should_receive(:message) { value }
    
    * 1 conversion
        from: pending
          to: skip
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/gem/updates_spec.rb
+++ spec/gem/updates_spec.rb
@@ -11,14 +11,14 @@ if test(?f, release_timestamp_file)
     context 'version file' do
       it 'has been updated since the last release' do
         version_time = File.stat('lib/reek.rb').mtime
-        (version_time > @release_time).should be_true
+        expect(version_time > @release_time).to be_truthy
       end
     end
 
     context 'history file' do
       it 'has been updated since the last release' do
         history_time = File.stat('History.txt').mtime
-        (history_time > @release_time).should be_true
+        expect(history_time > @release_time).to be_truthy
       end
     end
   end
